308074c39c69a9b5daaff2bf3779130c215bb8c4
Diego Magdaleno <diegommagdaleno@gmail.com>
2022-12-04 12:26:17 -0300

    busca dados da API

--- scripts/tabela.js
+++ scripts/tabela.js
@@ -1,37 +1,50 @@
-let alunos = JSON.parse(localStorage.getItem('alunos'))
+const url = 'http://ntcursoapi-env.eba-hvwnzgx7.us-east-1.elasticbeanstalk.com/nt-curso-api/alunos/'
 
-let tabela = document.getElementById('tabela-alunos')
-
-alunos.forEach(function(aluno, indice) {
-    let linha = tabela.insertRow()
-    let nome = linha.insertCell(0)
-    nome.innerHTML = aluno.nome
-    let idade = linha.insertCell(1)
-    idade.innerHTML = aluno.idade
-    let email = linha.insertCell(2)
-    email.innerHTML = aluno.email
-
-    var img = document.createElement('img')
-    img.src = './imagens/apagar.png'
-    img.style.width = '15px'
-    let acoes = linha.insertCell(3)
-    acoes.appendChild(img)
-
-    var imgEditar = document.createElement('img')
-    imgEditar.src = './imagens/editar.png'
-    imgEditar.style.width = '15px'
-    acoes.appendChild(imgEditar)
-
-    img.addEventListener('click', () => apagar(indice))
-    imgEditar.addEventListener('click', () => editar(indice))
+fetch(url).then(function (resposta) {
+    console.log('Sucesso!', resposta.status)
+    return resposta.json()
 })
+    .then(function (alunos) {
+        let tabela = document.getElementById('tabela-alunos')
+
+        alunos.forEach(function (aluno, indice) {
+            let linha = tabela.insertRow()
+            let nome = linha.insertCell(0)
+            nome.innerHTML = aluno.nome
+            let idade = linha.insertCell(1)
+            idade.innerHTML = aluno.idade
+            let email = linha.insertCell(2)
+            email.innerHTML = aluno.email
+
+            var imgApagar = document.createElement('img')
+            imgApagar.src = './imagens/apagar.png'
+            imgApagar.style.width = '15px'
+            let acoes = linha.insertCell(3)
+            acoes.appendChild(imgApagar)
+
+            var imgEditar = document.createElement('img')
+            imgEditar.src = './imagens/editar.png'
+            imgEditar.style.width = '15px'
+            acoes.appendChild(imgEditar)
+
+            imgApagar.addEventListener('click', () => apagar(aluno.id))
+            imgEditar.addEventListener('click', () => editar(aluno.id))
+        })
+    })
+    .catch(function (erro) {
+        console.warn('Algo deu errado', erro)
+    });
+
+
 
 function editar(indice) {
     window.location.href = './cadastro.html?indice=' + indice
 }
 
-function apagar(indice) {
-    alunos.splice(indice, 1)
-    localStorage.setItem('alunos', JSON.stringify(alunos))
-    document.location.reload()
+function apagar(id) {
+    fetch(url + id, { method: 'DELETE' })
+        .then(function(resposta) {
+            console.log('Apagado com sucesso', resposta)
+            document.location.reload()
+        })
 }
